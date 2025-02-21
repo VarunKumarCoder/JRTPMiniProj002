76867ba21f50e4a24e0aacc08f228f26ae95f633
VarunKumarCoder <medavarunkumar@gmail.com>
2025-02-21 12:03:06 +0530

    Reposoitory Interface added as a Has_A property as dependency injection	P

--- JRTP701-MiniProj02-ReportAPII/src/main/java/com/nt/service/CourseMgmtServiceImpl.java
+++ JRTP701-MiniProj02-ReportAPII/src/main/java/com/nt/service/CourseMgmtServiceImpl.java
@@ -3,13 +3,19 @@ package com.nt.service;
 import java.util.List;
 import java.util.Set;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.nt.model.SearchInputs;
 import com.nt.model.SearchResults;
+import com.nt.repository.ICourseDetailsRepository;
 
 import jakarta.servlet.http.HttpServletResponse;
 
+@Service("courseService")
 public class CourseMgmtServiceImpl implements ICourseMgmtService {
-
+	@Autowired
+	private ICourseDetailsRepository courseRepo;
 	@Override
 	public Set<String> showAllCourseCategories() {
 		// TODO Auto-generated method stub
