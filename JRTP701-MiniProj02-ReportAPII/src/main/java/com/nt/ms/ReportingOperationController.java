afa63d3e8a8b23732ee78dce900c933a2267fdc6
VarunKumarCoder <medavarunkumar@gmail.com>
2025-02-22 11:52:24 +0530

    Search using filters Api Completed

--- JRTP701-MiniProj02-ReportAPII/src/main/java/com/nt/ms/ReportingOperationController.java
+++ JRTP701-MiniProj02-ReportAPII/src/main/java/com/nt/ms/ReportingOperationController.java
@@ -1,14 +1,19 @@
 package com.nt.ms;
 
+import java.util.List;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.nt.model.SearchInputs;
+import com.nt.model.SearchResults;
 import com.nt.service.ICourseMgmtService;
 
 @RestController
@@ -45,5 +50,16 @@ public class ReportingOperationController {
 				return new ResponseEntity<String>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
 			}
 		}
+		
+		@PostMapping("/search")
+		public ResponseEntity<?> fetchCoursesByFilters(@RequestBody SearchInputs inputs){
+			try {
+				List<SearchResults> list=service.showCoursesByFilters(inputs);
+				return new ResponseEntity<List<SearchResults>>(list, HttpStatus.OK);
+			} catch (Exception e) {
+				return new ResponseEntity<String>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+			}
+		}
+		
 		
 }
