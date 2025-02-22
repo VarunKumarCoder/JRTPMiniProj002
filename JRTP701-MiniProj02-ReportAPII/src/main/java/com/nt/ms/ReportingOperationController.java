6494f6f76376465069ae3a30e4de1098e34dc8ce
VarunKumarCoder <medavarunkumar@gmail.com>
2025-02-22 11:24:17 +0530

    show all categories API Done

--- JRTP701-MiniProj02-ReportAPII/src/main/java/com/nt/ms/ReportingOperationController.java
+++ JRTP701-MiniProj02-ReportAPII/src/main/java/com/nt/ms/ReportingOperationController.java
@@ -1,6 +1,11 @@
 package com.nt.ms;
 
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +17,13 @@ public class ReportingOperationController {
 		@Autowired
 	private ICourseMgmtService service;
 		
-		
+		@GetMapping("/courses")
+		public ResponseEntity<?> fetchCourseCategories(){
+			try {
+				Set<String> courseInfo=service.showAllCourseCategories();
+				return new ResponseEntity<Set<String>>(courseInfo, HttpStatus.OK);
+			} catch (Exception e) {
+				return new ResponseEntity<String>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+			}
+		}
 }
